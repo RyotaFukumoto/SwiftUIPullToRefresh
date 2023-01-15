b6dbc6e84238945c5f6bb59801d84d491b8185a5
RyotaFukumoto <f.ryota.828@gmail.com>
2023-01-15 22:09:39 +0900

    Pull To Refresh

--- SwiftUIPullToRefresh/ContentView.swift
+++ SwiftUIPullToRefresh/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    struct Cat: Identifiable {
+        var id: Int
+        let name: String
+    }
+    let cats: [Cat] = [Cat(id: 1, name: "ひじき"),Cat(id: 2, name: "つくし")]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(cats) { cat in
+                Text("\(cat.name)")
+                    .padding()
+            }
         }
-        .padding()
+    .refreshable {
+            do {
+                try await Task.sleep(nanoseconds: 2 * 1_000_000_000)
+            } catch {}
+        }
     }
 }
 
