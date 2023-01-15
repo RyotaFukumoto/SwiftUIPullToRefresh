8cbdcfa927dadf65a79b2ae19103a56ca4a5959e
RyotaFukumoto <f.ryota.828@gmail.com>
2023-01-15 21:45:59 +0900

    Initial Commit

---
+++ SwiftUIPullToRefresh/SwiftUIPullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPullToRefreshApp.swift
+//  SwiftUIPullToRefresh
+//
+//  Created by ryota on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIPullToRefreshUITests/SwiftUIPullToRefreshUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIPullToRefreshUITestsLaunchTests.swift
+//  SwiftUIPullToRefreshUITests
+//
+//  Created by ryota on 2023/01/15.
+//
+
+import XCTest
+
+final class SwiftUIPullToRefreshUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
